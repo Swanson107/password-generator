856060a66c0b9b7b663b0a8aba5a60305499b156
Thomas S <thomas.swanson107@gmail.com>
2023-04-05 21:44:17 -0700

    Add password generator

---
+++ generator.js
@@ -0,0 +1,31 @@
+
+console.log("Init")
+
+function passGen() {
+    if (numInput.value > 64) {
+        numInput.value = 64;
+    }
+    console.log("GENNING");
+    passField.textContent = "";
+    newPass = "";
+    const num = numInput.value;
+    for (i=0;i<num;i++){
+        randomPick = genText.charAt(Math.floor(Math.random() * genText.length));
+        newPass = newPass + randomPick;
+    }
+    console.log(newPass)
+    passField.textContent = newPass;
+}
+
+
+const genText = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+let newPass = "";
+let passLength = 0;
+const passField = document.getElementById('result')
+const submitBtn = document.getElementById('input-button');
+const numInput = document.getElementById('input');
+
+
+submitBtn.addEventListener('click', passGen);
+
